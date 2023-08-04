77f24a1d32ccf9c24bffdbfa60c005c2c29b98e4
Muhammad Shaban <m.shaaban@justyol.com>
2023-08-04 20:20:34 +0300

    add angular material theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +10,14 @@ import { LoaderComponent } from './core/components/loader/loader.component';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, LoaderComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        LoaderComponent,
+        MatToolbarModule,
+        MatIconModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
