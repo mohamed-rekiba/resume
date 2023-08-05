1203a3f3e4c9f14127a0195decc608f179fb928a
Muhammad Shaban <m.shaaban@justyol.com>
2023-08-05 20:35:52 +0300

    initiat experience section

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,10 +6,10 @@ import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './home.component';
 import { SidebarComponent } from './partials/sidebar/sidebar.component';
 import { HomeRoutingModule } from './home-routing.module';
-import { ResumeComponent } from './partials/resume/resume.component';
+import { ExperienceComponent } from './partials/experience/experience.component';
 
 @NgModule({
-    declarations: [HomeComponent, SidebarComponent, ResumeComponent],
+    declarations: [HomeComponent, SidebarComponent, ExperienceComponent],
     imports: [CommonModule, MatButtonModule, MatIconModule, HomeRoutingModule],
 })
 export class HomeModule {}
